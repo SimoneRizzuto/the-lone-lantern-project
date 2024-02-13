12cecf912a7ed18da36ab81da9e3112f90067491
Simorico <simorico.dev@gmail.com>
2024-02-13 21:49:01 +1000

    Imported attack sprite sheet, added timer

--- Scenes/Player/Player.cs
+++ Scenes/Player/Player.cs
@@ -1,76 +1,37 @@
 using Godot;
 using System;
 using TheLoneLanternProject.Constants;
+using TheLoneLanternProject.Scenes.Player;
 
 public partial class Player : CharacterBody2D
 {
     [Export] public int Speed = 5000;
 
-    public Direction CurrentDirection = Direction.Right;
+    public Direction Direction = Direction.Right;
     public PlayerState State = PlayerState.Idle;
 
     private AnimatedSprite2D _sprite = new();
     private CollisionShape2D _collisionShape = new();
-    
-    
-    public enum Direction
-    {
-        Left,
-        Right
-    }
-    public enum PlayerState
-    {
-        Idle,
-        Walk,
-        Attack,
-        Hurt,
-        Disabled
-    }
+    private Timer _attackTimer = new();
 
     public override void _Ready()
     {
         _sprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
         _collisionShape = GetNode<CollisionShape2D>("CollisionShape2D");
+        _attackTimer = GetNode<Timer>("AttackTimer");
     }
 
     public override void _Process(double delta)
     {
         SetDirection();
-
-        if (State == PlayerState.Idle)
-        {
-            _sprite.Animation = "idle";
-        }
-        else if (State == PlayerState.Walk)
-        {
-            _sprite.Animation = "walk";
-            _sprite.Play();
-        }
-
-        if (CurrentDirection == Direction.Left)
-        {
-            _sprite.FlipH = true;
-        }
-        else if (CurrentDirection == Direction.Right)
-        {
-            _sprite.FlipH = false;
-        }
+        SetPlayerState();
+        SetFlipH();
+        SetAnimation();
     }
-
-    private void SetDirection()
-    {
-        if (Input.IsActionPressed(InputMapAction.Left))
-        {
-            CurrentDirection = Direction.Left;
-        }
-        else if (Input.IsActionPressed(InputMapAction.Right))
-        {
-            CurrentDirection = Direction.Right;
-        }
-    }
-
     public override void _PhysicsProcess(double delta)
     {
+        if (State == PlayerState.Attack) return;
+        
         var vector = Input.GetVector(InputMapAction.Left, InputMapAction.Right, InputMapAction.Up, InputMapAction.Down);
         var position = vector * Speed * (float)delta;
         
@@ -86,5 +47,65 @@ public partial class Player : CharacterBody2D
         }
         
         MoveAndSlide();
+    }
+
+    private void SetDirection()
+    {
+        if (Input.IsActionPressed(InputMapAction.Left))
+        {
+            Direction = Direction.Left;
+        }
+        else if (Input.IsActionPressed(InputMapAction.Right))
+        {
+            Direction = Direction.Right;
+        }
+    }
+
+    private void SetPlayerState()
+    {
+        if (Input.IsActionPressed(InputMapAction.Attack))
+        {
+            State = PlayerState.Attack;
+            _attackTimer.Start();
+        }
+    }
+
+    private void SetFlipH()
+    {
+        if (State == PlayerState.Attack) return;
+        
+        if (Direction == Direction.Left)
+        {
+            _sprite.FlipH = true;
+        }
+        else if (Direction == Direction.Right)
+        {
+            _sprite.FlipH = false;
+        }
+    }
+
+    private void SetAnimation()
+    {
+        if (State == PlayerState.Idle)
+        {
+            _sprite.Animation = "idle";
+        }
+        else if (State == PlayerState.Walk)
+        {
+            _sprite.Animation = "walk";
+            _sprite.Play();
+        }
+        else if (State == PlayerState.Attack)
+        {
+            _sprite.Animation = "attack";
+            _sprite.Play();
+        }
+    }
+
+    
+    // Signal Events
+    public void OnAttackTimerTimeout()
+    {
+        State = PlayerState.Idle;
     }
 }
