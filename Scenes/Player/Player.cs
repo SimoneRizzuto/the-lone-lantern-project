221bf8848b585da23295faed3b50cd5a52e0a194
Simorico <simorico.dev@gmail.com>
2024-03-26 23:37:27 +1000

    Added OnAttackShapeAreaEntered
    Plan to replace Signal method later for better implementation of damage detection

--- Scenes/Player/Player.cs
+++ Scenes/Player/Player.cs
@@ -209,4 +209,14 @@ public partial class Player : CharacterBody2D
     {
         healthState = StaminaHealthState.Regen;
     }
+    public void OnAttackShapeAreaEntered(Node2D area)
+    {
+        GD.Print("Area Entered: Attack");
+        if (area.IsInGroup("enemies"))
+        {
+            var enemy = (TheLoneLanternProject.Scenes.Enemies.Enemy)area;
+            enemy.TakeDamage(1);
+        }
+
+    }
 }
