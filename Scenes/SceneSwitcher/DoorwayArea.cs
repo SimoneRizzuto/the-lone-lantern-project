455f80202444d9c5ea10e5fea0583a2df63c99bb
Dominic Cotter <dominiccotter17@outlook.com>
2024-04-27 14:41:43 +1000

    Started logic cleanup

--- Scenes/SceneSwitcher/DoorwayArea.cs
+++ Scenes/SceneSwitcher/DoorwayArea.cs
@@ -3,13 +3,12 @@ using TheLoneLanternProject.Constants;
 
 public partial class DoorwayArea : Area2D
 {
-    private bool bodyEntered = false;
-
     [Export] public PackedScene NewScene { get; set; }
 
     [Export] public string DoorName;
 
     private CustomSignals customSignals;
+    private bool bodyEntered = false;
 
     public override void _Ready()
     {
@@ -17,24 +16,14 @@ public partial class DoorwayArea : Area2D
         
     }
 
-    private void OnBodyEntered(PhysicsBody2D body)
-    {
-        if (body.IsInGroup("player"))
-        {
-            bodyEntered = true;
-        }
-        
-    }
-
-    private void OnBodyExited(PhysicsBody2D body)
-    {
-        if (body.IsInGroup("player"))
-        {
-            bodyEntered = false;
-        }
-    }
-
     public override void _Process(double delta)
+    {
+        TriggerTransition();
+    }
+
+    
+
+    private void TriggerTransition()
     {
         if (bodyEntered)
         {
@@ -43,7 +32,23 @@ public partial class DoorwayArea : Area2D
                 customSignals.EmitSignal(nameof(CustomSignals.SceneSwitch), NewScene, DoorName);
             }
         }
+    }
 
-        // Future implementation requires that this not be predatory i.e. button hold not single press.
+    // Signal Events
+    private void OnBodyEntered(PhysicsBody2D body)
+    {
+        if (body.IsInGroup("player"))
+        {
+            bodyEntered = true;
+        }
+
+    }
+
+    private void OnBodyExited(PhysicsBody2D body)
+    {
+        if (body.IsInGroup("player"))
+        {
+            bodyEntered = false;
+        }
     }
 }
--- Scenes/SceneSwitcher/SceneSwitcher.cs
+++ Scenes/SceneSwitcher/SceneSwitcher.cs
@@ -4,15 +4,14 @@ using System;
 
 public partial class SceneSwitcher : Node
 {
+    private CustomSignals customSignals = new CustomSignals(); 
+    
     private Dictionary doorDictionary = new Godot.Collections.Dictionary
     {
         {"RightDoor", "StartPositionLeft"},
         {"LeftDoor", "StartPositionRight"},
         {"HouseDoor", "StartPosition"}
     };
-
-    //Listen for emitted signal
-    private CustomSignals customSignals = new CustomSignals();
 
     public override void _Ready()
     {
@@ -24,39 +23,29 @@ public partial class SceneSwitcher : Node
     {
 
         // Get the starting position based on the doorname
-        var startPositionString = doorDictionary[DoorName];
-        if (startPositionString.AsStringName() == "")
+        var startPositionNodeString = doorDictionary[DoorName];
+        if (startPositionNodeString.AsStringName() == "")
         {
             GD.PrintErr("DoorName from DoorwayArea does not match any corresponding key in doorDictionary.");
         }
         else
         {
-            // Assuming that the scene switcher node is the top and the current scene the player is in is the first child node
             var currentSceneName = GetChild(0);
-
             var nextSceneName = NewScene.Instantiate();
             RemoveChild(currentSceneName);
             AddChild(nextSceneName);
-
-
-
-            // Set the player to the startPosition if there is one
+            
             var player = GetNode<Node2D>("./" + nextSceneName.Name + "/GameContainer/PlayerController");
             try
             {
-                var startPosition = GetNode<Node2D>("./" + nextSceneName.Name + "/GameContainer/" + startPositionString);
-                player.Position = startPosition.Position;
+                var newStartPosition = GetNode<Node2D>("./" + nextSceneName.Name + "/GameContainer/" + startPositionNodeString);
+                player.Position = newStartPosition.Position;
             }
             catch
             {
 
             }
         }
-
-
-        
-        
-
 
     }
 
