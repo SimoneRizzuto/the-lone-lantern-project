6cbab3114e7c037fa3f70dad0ad8414f63cbfa08
Simorico <simorico.dev@gmail.com>
2024-05-04 16:52:29 +1000

    Added logic for exit direction when entering door

--- Scenes/SceneSwitcher/SceneSwitcher.cs
+++ Scenes/SceneSwitcher/SceneSwitcher.cs
@@ -17,48 +17,42 @@ public partial class SceneSwitcher : Node
 
     private void HandleSceneSwitch(DoorSpawnAttributes attributes)
     {
-        try
+        var sceneToRemove = GetChild(0);
+        RemoveChild(sceneToRemove);
+        
+        var uid = ResourceUid.TextToId(attributes.NewSceneUid);
+        var path = ResourceUid.GetIdPath(uid);
+        var nextScenePackedScene = (PackedScene)ResourceLoader.Load(path);
+        if (nextScenePackedScene == null)
         {
-            var sceneToRemove = GetChild(0);
-            RemoveChild(sceneToRemove);
-            
-            var uid = ResourceUid.TextToId(attributes.NewSceneUid);
-            var path = ResourceUid.GetIdPath(uid);
-            var nextScenePackedScene = (PackedScene)ResourceLoader.Load(path);
-            if (nextScenePackedScene == null)
-            {
-                GD.PrintErr($"Scene cannot be found. UID: {attributes.NewSceneUid} - DoorName: {attributes.DoorName}");
-                return;
-            }
-            
-            var sceneToAdd = nextScenePackedScene.Instantiate();
-            AddChild(sceneToAdd);
-            
-            var tree = GetTree();
-            
-            // Find door by group name "door" and the "doorName".
-            var doorNodes = tree.GetNodesInGroup(NodeGroup.Door);
-            var door = doorNodes.Cast<Door2D>().FirstOrDefault(x => x.DoorName == attributes.DoorName);
-            if (door == null)
-            {
-                GD.PrintErr($"Door cannot be found. UID: {attributes.NewSceneUid} - DoorName: {attributes.DoorName}");
-                return;
-            }
-            
-            // Find player by group name "player".
-            var playerNode = tree.GetNodesInGroup(NodeGroup.Player).FirstOrDefault();
-            if (playerNode == null)
-            {
-                GD.PrintErr($"Player cannot be found. UID: {attributes.NewSceneUid} - DoorName: {attributes.DoorName}");
-                return;
-            }
-            
-            var playerNode2D = (Node2D)playerNode;
-            playerNode2D.Position = door.GlobalPosition;
+            GD.PrintErr($"{nameof(nextScenePackedScene)} cannot be found. UID: {attributes.NewSceneUid} - DoorName: {attributes.DoorName}");
+            return;
         }
-        catch (Exception ex)
+        
+        var sceneToAdd = nextScenePackedScene.Instantiate();
+        AddChild(sceneToAdd);
+        
+        var tree = GetTree();
+        
+        // Find door by group name "door" and the "doorName".
+        var doorNodes = tree.GetNodesInGroup(NodeGroup.Door);
+        var door = doorNodes.Cast<Door2D>().FirstOrDefault(x => x.DoorName == attributes.DoorName);
+        if (door == null)
         {
-            GD.PrintErr($"Door navigation failed: {ex.Message}");
+            GD.PrintErr($"{nameof(door)} was null. UID: {attributes.NewSceneUid} - DoorName: {attributes.DoorName}");
+            return;
         }
+        
+        // Find player by group name "player".
+        var playerNode = tree.GetNodesInGroup(NodeGroup.Player).FirstOrDefault();
+        if (playerNode == null)
+        {
+            GD.PrintErr($"{nameof(playerNode)} was null. UID: {attributes.NewSceneUid} - DoorName: {attributes.DoorName}");
+            return;
+        }
+        
+        var player = (Player.Player)playerNode;
+        player.GlobalPosition = door.GlobalPosition;
+        player.Direction = attributes.ExitDirection;
     }
 }
