80321197b690989cc754f33263356acf7687cde3
Dominic Cotter <dominiccotter17@outlook.com>
2024-08-06 19:30:40 +1000

    Create Options Menu

---
+++ UI/OptionsMenu.cs
@@ -0,0 +1,15 @@
+using Godot;
+using System;
+
+public partial class OptionsMenu : Control
+{
+    public override void _Ready()
+    {
+        Hide();
+    }
+
+    public void OnBackPressed()
+    {
+        //Code to return to the pause menu
+    }
+}
