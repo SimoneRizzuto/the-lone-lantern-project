e4fc75b464b515d487710629166f413a73ad069d
Dominic Cotter <dominiccotter17@outlook.com>
2024-08-06 18:34:56 +1000

    Added non-functional UI.
    Added in the pause menu UI. No functionality yet.

--- UI/UI.cs
+++ UI/UI.cs
@@ -4,6 +4,14 @@ using TheLoneLanternProject.Constants;
 public partial class UI : CanvasLayer
 {
     public bool Playing = true;
+
+    public override void _Ready()
+    {
+        Hide();
+    }
+
+
+    // I think that this should go somewhere more important maybe???
     public override void _Process(double delta)
     {
         if (Input.IsActionPressed(InputMapAction.Pause))
@@ -11,6 +19,7 @@ public partial class UI : CanvasLayer
             if (Playing) {
                 GetTree().Paused = true;
                 Playing = !Playing;
+                Show();
                 return;
                 // Send signal to show or just show the pause menu UI which will have the resume button. 
                 // Right now pressing this will be super annnoying and laggy when pausing and  unpausing. 
@@ -20,6 +29,7 @@ public partial class UI : CanvasLayer
             {
                 GetTree().Paused = false;
                 Playing = !Playing;
+                Hide();
                 return;
             }
             
