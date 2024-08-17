3e77ce1c7500b4b05eb0cb0ed60e5e10c3692215
Dominic Cotter <dominiccotter17@outlook.com>
2024-08-17 20:43:43 +1000

    Added working functionality to window screen and resolution selection.

--- UI/OptionsMenu.cs
+++ UI/OptionsMenu.cs
@@ -1,17 +1,132 @@
 using Godot;
-using TheLoneLanternProject.Constants;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text.RegularExpressions;
 
 public partial class OptionsMenu : Control
 {
     private CustomSignals customSignals = new();
+    private int viewportHeight = new();
+    private int viewportWidth = new();
+    private string viewportMode = "";
+
+    private Godot.Collections.Dictionary<string, Vector2I> resolutionDict = new();
+    private Godot.Collections.Array<string> windowScreenArray = new();
+
+
 
     public override void _Ready()
     {
         customSignals = GetNode<CustomSignals>("/root/CustomSignals");
+        
+        AddResolutionOptions();
+        OptionButton resolutionOptionButton = GetNode<OptionButton>("./MarginContainer/VBoxContainer/SettingsTab/TabContainer/Visual/MarginContainer/VBoxContainer/Res"); // This needs to change & is bad practice
+        resolutionOptionButton.ItemSelected += resolutionIndex => ChangeResolution((int) resolutionIndex);
+
+        viewportHeight = (int)ProjectSettings.GetSetting("display/window/size/viewport_height");
+        viewportWidth = (int)ProjectSettings.GetSetting("display/window/size/viewport_width");
+        string key = $"{viewportWidth} x {viewportHeight}";
+
+
+        if (resolutionDict.ContainsKey(key))
+        {
+            resolutionOptionButton.Selected = resolutionDict.Keys.ToList().IndexOf(key);
+        }
+        else
+        {
+            resolutionOptionButton.Selected = -1; 
+
+        }
+
+        AddWindowScreenOptions();
+        OptionButton windowOptionButton = GetNode<OptionButton>("./MarginContainer/VBoxContainer/SettingsTab/TabContainer/Visual/MarginContainer/VBoxContainer/WinScn"); // This needs to change & is bad practice
+        windowOptionButton.ItemSelected += windowScreenIndex => ChangeWindowScreen((int)windowScreenIndex);
+
+        viewportMode = GetViewport().GetWindow().Mode.ToString();
+        if (viewportMode == "ExclusiveFullscreen"){
+            viewportMode = "Exclusive Fullscreen";
+        }
+        if (windowScreenArray.Contains(viewportMode))
+        {
+            
+            windowOptionButton.Selected = windowScreenArray.IndexOf(viewportMode); 
+        }
+        else
+        {
+            windowOptionButton.Selected = -1;
+        }
     }
+
+
+    private void WindowScreenOptions()
+    {
+        windowScreenArray.Add("Windowed");
+        windowScreenArray.Add("Fullscreen");
+        windowScreenArray.Add("Exclusive Fullscreen");
+
+    }
+
+    public void AddWindowScreenOptions()
+    {
+        WindowScreenOptions();
+        var windowOptionButton = GetNode<OptionButton>("./MarginContainer/VBoxContainer/SettingsTab/TabContainer/Visual/MarginContainer/VBoxContainer/WinScn"); // This needs to change & is bad practice
+        //GD.Print(GetTree().Root.GetTreeString());
+        foreach (var screen in windowScreenArray)
+        {
+            windowOptionButton.AddItem(screen);
+        }
+    }
+
+    private void ChangeWindowScreen(int windowScreenIndex)
+    {
+        switch (windowScreenIndex)
+        {
+            case 0:
+                DisplayServer.WindowSetMode(DisplayServer.WindowMode.Windowed); 
+                break;
+            case 1:
+                DisplayServer.WindowSetMode(DisplayServer.WindowMode.Fullscreen);
+                break;
+            case 2:
+                DisplayServer.WindowSetMode(DisplayServer.WindowMode.ExclusiveFullscreen);
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void ResolutionOptions()
+    {
+        resolutionDict.Add("1024 x 576", new Vector2I(1024, 576));
+        resolutionDict.Add("1152 x 648", new Vector2I(1152, 648));
+        resolutionDict.Add("1280 x 720", new Vector2I(1280, 720));
+        resolutionDict.Add("1345 x 790", new Vector2I(1345, 790)); // Let's add a weird one to check the scaling
+        resolutionDict.Add("1920 x 1080", new Vector2I(1920, 1080));
+        resolutionDict.Add("2560 x 1440", new Vector2I(2560, 1440));
+        resolutionDict.Add("3840 x 2160", new Vector2I(3840, 2160));
+    }
+
+
+    public void AddResolutionOptions()
+    {
+        ResolutionOptions();
+        var resolutionOptionButton = GetNode<OptionButton>("./MarginContainer/VBoxContainer/SettingsTab/TabContainer/Visual/MarginContainer/VBoxContainer/Res"); // This needs to change & is bad practice
+        //GD.Print(GetTree().Root.GetTreeString());
+        foreach (var resolution in resolutionDict)
+        {
+            resolutionOptionButton.AddItem(resolution.Key);
+        }
+    }
+
+    private void ChangeResolution(int resolutionIndex)
+    {
+        DisplayServer.WindowSetSize(resolutionDict.Values.ToList()[resolutionIndex]);
+    }
+
 
     public void OnBackPressed()
     {
         customSignals.EmitSignal(nameof(CustomSignals.MenuSwitch), "Pause");
     }
+
 }
