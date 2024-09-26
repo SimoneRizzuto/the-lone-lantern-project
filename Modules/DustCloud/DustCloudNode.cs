341347acf7b3366babad34b86930a682b7761c21
Simorico <simorico.dev@gmail.com>
2024-09-26 20:52:55 +1000

    Added DustCloudNode and DustCloudModule

---
+++ Modules/DustCloud/DustCloudNode.cs
@@ -0,0 +1,23 @@
+using Godot;
+
+namespace TheLoneLanternProject.Modules.DustCloud;
+public partial class DustCloudNode : Node2D
+{
+    private AnimatedSprite2D animatedSprite2D;
+    
+    public virtual void Play_DashCloud(Vector2? position = null)
+    {
+        if (animatedSprite2D == null)
+        {
+            animatedSprite2D ??= GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+        }
+        
+        animatedSprite2D.AnimationFinished += Despawn;
+        animatedSprite2D.Play("dash cloud");
+    }
+    
+    private void Despawn()
+    {
+        QueueFree();
+    }
+}
