9195e62a904ce3bedffe54aec9235bee613aaad8
Dominic Cotter <dominiccotter17@outlook.com>
2024-10-27 17:01:40 +1000

    Begin interactable node and modules setup.

---
+++ Modules/Interactables/Interactable.cs
@@ -0,0 +1,13 @@
+using Godot;
+using System;
+
+namespace TheLoneLanternProject.Modules;
+
+public partial class Interactable : Node
+{
+    
+    public override void _Ready() 
+    {
+        
+    }
+}
