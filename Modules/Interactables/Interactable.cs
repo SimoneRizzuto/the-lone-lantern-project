ce92e6788d580a9e0fd3f54ba98e0bd61a453aa9
Dominic Cotter <dominiccotter17@outlook.com>
2024-10-29 21:22:29 +1000

    Add PickUp Module. Prepare for testing of interaction system.

--- Modules/Interactables/Interactable.cs
+++ Modules/Interactables/Interactable.cs
@@ -1,13 +1,23 @@
 using Godot;
 using System;
+using TheLoneLanternProject.Constants;
+using TheLoneLanternProject.Helpers;
 
 namespace TheLoneLanternProject.Modules;
 
 public partial class Interactable : Node
 {
-    
-    public override void _Ready() 
+    private CustomSignals customSignals = new();
+
+    public override void _Ready()
     {
-        
+        customSignals = GetNode<CustomSignals>("/root/CustomSignals");
+    }
+    private void OnInteractionZoneBodyEntered(PhysicsBody2D body)
+    {
+        if (body.IsInGroup(NodeGroup.Player) && Input.IsActionJustPressed(InputMapAction.Interact))
+        {
+            customSignals.EmitSignal(nameof(CustomSignals.InteractionEventHandler));
+        }
     }
 }
