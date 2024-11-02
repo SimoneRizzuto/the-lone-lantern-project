d7981016cc8a785e9c6274aa31160eb6de1f2428
Dominic Cotter <dominiccotter17@outlook.com>
2024-11-02 21:11:31 +1000

    Fixed issue of being unable to move the interactable are/collisionshape
    Converted the type of node from Node to Node2D which enabled the child to be moved with the parent.

--- Modules/Interactables/Interactable.cs
+++ Modules/Interactables/Interactable.cs
@@ -1,24 +1,48 @@
 using Godot;
 using System;
 using TheLoneLanternProject.Constants;
-using TheLoneLanternProject.Helpers;
 
 namespace TheLoneLanternProject.Modules;
 
-public partial class Interactable : Node
+public partial class Interactable : Node2D
 {
     private CustomSignals customSignals;
+    private bool bodyEntered = false;
 
     public override void _Ready()
     {
         customSignals = GetNode<CustomSignals>("/root/CustomSignals");
     }
+
+    
+
+    public override void _Process(double delta)
+    {
+        if (bodyEntered)
+        {
+            if (Input.IsActionJustPressed(InputMapAction.Interact))
+            {
+                customSignals.EmitSignal(nameof(CustomSignals.Interaction));
+                GD.Print("Sent!");
+            }
+        }
+        
+    }
     private void OnInteractionZoneBodyEntered(PhysicsBody2D body)
     {
-        if (body.IsInGroup(NodeGroup.Player) && Input.IsActionJustPressed(InputMapAction.Interact))
+        if (body.IsInGroup(NodeGroup.Player) )
         {
-            customSignals.EmitSignal(nameof(CustomSignals.Interaction));
-            GD.Print("Sent!");
+            bodyEntered = true;
         }
     }
+
+    private void OnInteractionZoneBodyExited(PhysicsBody2D body) 
+    {
+        if (body.IsInGroup(NodeGroup.Player))
+        {
+            bodyEntered = false;
+        }
+    }
+
+
 }
