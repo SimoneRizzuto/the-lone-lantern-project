9a63968ad3f1d8b5495fea199173da4031b16d72
Simorico <simorico.dev@gmail.com>
2025-03-07 23:43:45 +1000

    added logic to OutOfCombatMove

--- Scripts/Modules/Enemy/Basic/BasicOutOfCombatMoveBehaviour.cs
+++ Scripts/Modules/Enemy/Basic/BasicOutOfCombatMoveBehaviour.cs
@@ -1,9 +1,28 @@
+using System;
 using Godot;
+using System.Diagnostics;
+using TheLoneLanternProject.Scripts.Shared.Constants;
 
 namespace TheLoneLanternProject.Scripts.Modules.Enemy.Basic;
 
 [GlobalClass]
 public partial class BasicOutOfCombatMoveBehaviour : BaseEnemyBehaviour
 {
+    private Stopwatch stopwatch = new();
     
+    public override void _PhysicsProcess(double delta)
+    {
+        if (StateMachine.EnemyState != EnemyState.OutOfCombatIdle) return;
+        
+        if (!stopwatch.IsRunning)
+        {
+            stopwatch.Restart();
+        }
+        
+        var seconds = 3; // make random number
+        if (stopwatch.Elapsed == TimeSpan.FromSeconds(seconds))
+        {
+            StateMachine.EnemyState = EnemyState.OutOfCombatIdle;
+        }
+    }
 }
