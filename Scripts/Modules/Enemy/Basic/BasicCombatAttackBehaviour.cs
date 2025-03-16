7ff385501e79c46086cd4b56bd62f27b16593c28
Simorico <simorico.dev@gmail.com>
2025-03-16 10:16:52 +1000

    added attack logic

--- Scripts/Modules/Enemy/Basic/BasicCombatAttackBehaviour.cs
+++ Scripts/Modules/Enemy/Basic/BasicCombatAttackBehaviour.cs
@@ -1,3 +1,4 @@
+using System;
 using Godot;
 using TheLoneLanternProject.Scripts.Shared.Constants;
 
@@ -6,8 +7,42 @@ namespace TheLoneLanternProject.Scripts.Modules.Enemy.Basic;
 [GlobalClass]
 public partial class BasicCombatAttackBehaviour : BaseEnemyBehaviour
 {
+    private int attackSpeed = 10;
+    private bool applyingVelocity;
+    private Vector2 DirectionToPlayer => StateMachine.EnemyTemplate.Position.DirectionTo(Luce.Position);
+    
     public override void _PhysicsProcess(double delta)
     {
-        if (StateMachine.EnemyState is not EnemyState.CombatAttack) return;
+        if (StateMachine.EnemyState is not EnemyState.CombatAttack)
+        {
+            applyingVelocity = false;
+            return;
+        }
+
+        if (!Timer.IsRunning)
+        {
+            Timer.Restart();
+        }
+        
+        if (!applyingVelocity)
+        {
+            StateMachine.EnemyTemplate.CalculatedVelocity = DirectionToPlayer * EnemyConstants.AttackSpeed;
+            applyingVelocity = true;
+        }
+
+        if (Timer.Elapsed > TimeSpan.FromSeconds(0.25))
+        {
+            Timer.Reset();
+            StateMachine.EnemyState = EnemyState.CombatWait;
+        }
     }
+    
+    // attack objects can probably be static in here.
+    // each attack needs to be assignable for this Behavior to find it.
+    
+    // Because each Behaviour is custom, instancing the attacks statically is probably fine?
+    // experiment with two attacks in here
+    // consider: animations, hitboxes, length of attack, what state the enemy will go into
+    // all of this makes me think each shouldn't be a static object, but instead a PackedScene???
+    // and each packed scene gets added as a child or some other way??????
 }
